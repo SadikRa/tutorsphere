0ac7a6762fcc06f0ad5db5d7f53a45966129c6ee
SadikRa <sadikrahman494@gmail.com>
2025-03-03 22:10:30 +0600

    updated

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -11,72 +11,50 @@ interface UserWithRole extends User {
 }
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
-    // GithubProvider
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // GoogleProvider
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
     }),
-
-    // CredentialsProvider
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = await fetch(`${process.env.SERVER_URL}/api/auth/login`, {
+      async authorize(credentials) {
+        const response = await fetch(`${process.env.SERVER_URL}/api/auth/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(credentials),
-        }).then((res) => res.json());
+        });
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user.user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
+        const user = await response.json();
+        return user ? user.user : null;
       },
     }),
   ],
   callbacks: {
-    async jwt({ token, user }: { token: JWT; user: User }) {
+    async jwt({ token, user }: { token: JWT; user?: User }) {
       if (user) {
-        const typedUser = user as UserWithRole;
-        token.role = typedUser.role;
-        token.accessToken = typedUser.access_token;
+        Object.assign(token, {
+          role: (user as UserWithRole).role,
+          accessToken: (user as UserWithRole).access_token,
+        });
       }
-
       return token;
     },
     async session({ session, token }: { session: Session; token: JWT }) {
       if (session.user) {
-        const typesSession = session.user as UserWithRole;
-        typesSession.role = token.role as string;
+        (session.user as UserWithRole).role = token.role as string;
       }
-
       return session;
     },
   },
-  pages: {
-    signIn: "/login",
-  },
+  pages: { signIn: "/login" },
   secret: process.env.NEXTAUTH_SECRET,
 };
